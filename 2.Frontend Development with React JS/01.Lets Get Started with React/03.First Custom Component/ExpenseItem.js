042743e216d8e43336d4bd66c1d0e13dd47e0eee
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-05-17 18:15:31 +0530

    First Custom Component

---
+++ 2.Frontend Development with React JS/01.Lets Get Started with React/03.First Custom Component/ExpenseItem.js
@@ -0,0 +1,14 @@
+// Write your code here:
+
+function App() {
+
+  return (
+    <div>
+      <p>Food - $5</p>
+      <p>Petrol - $20</p>
+      <p>Movie - $10</p>
+    </div>
+  )
+  }
+
+export default App
