4e426c6f909ea3a71490888992a674284b23afd3
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-05-18 19:41:34 +0530

    Writing Complex JSX

---
+++ 2.Frontend Development with React JS/01.Lets Get Started with React/04.Writing Complex JSX/ExpenseItem.js
@@ -0,0 +1,18 @@
+// Write your code here:
+function App() {
+  return (
+    <div>
+      <div>
+        15 August 2023
+      </div>
+      <div>
+        <h2>Book</h2>
+        <div>$10</div>
+      </div>
+      <div>
+        Delhi
+      </div>
+    </div>
+  )
+}
+export default App
