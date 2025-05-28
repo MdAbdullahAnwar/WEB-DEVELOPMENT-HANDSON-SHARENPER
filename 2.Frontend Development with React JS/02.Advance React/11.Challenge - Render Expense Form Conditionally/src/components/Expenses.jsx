ada01f098c5a7ee8c27bac384e65a8eb1b747af1
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-05-29 01:04:00 +0530

    Challenge - Render Expense Form Conditionally

---
+++ 2.Frontend Development with React JS/02.Advance React/11.Challenge - Render Expense Form Conditionally/src/components/Expenses.jsx
@@ -0,0 +1,31 @@
+// Write your code at relevant places in the code below:
+
+import React, { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
+  );
+};
+
+export default Expenses;
