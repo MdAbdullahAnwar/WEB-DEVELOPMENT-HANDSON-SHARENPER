b5b19eb833ebbad0bc172ba0f5219f0c0857bd2c
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-05-30 00:11:42 +0530

    Higher Order Components

---
+++ 2.Frontend Development with React JS/02.Advance React/13.Higher Order Component/src/withLoader.js
@@ -0,0 +1,28 @@
+// withLoader.js
+import React, { useState, useEffect } from 'react';
+
+const withLoader = (WrappedComponent, data) => {
+//Write your code here and create your HOC 
+//Just like in the video  
+
+  return function withLoaderComponent(props) {
+    const [isLoading, setIsLoading] = useState(true);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setItems(data);
+        setIsLoading(false);
+      }, 2000);
+    }, []);
+
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return <WrappedComponent items={items} />
+  };
+
+};
+
+export default withLoader;
