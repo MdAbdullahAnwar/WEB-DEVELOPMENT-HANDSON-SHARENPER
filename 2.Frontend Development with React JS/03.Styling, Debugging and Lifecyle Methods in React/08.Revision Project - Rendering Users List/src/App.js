1a633c43831c48ab674f7f44b6302d5ab879c54d
MdAbdullahAnwar <abdullahanwar2109@gmail.com>
2025-06-03 16:46:21 +0530

    Revision Project - Rendering Users List

---
+++ 2.Frontend Development with React JS/03.Styling, Debugging and Lifecyle Methods in React/08.Revision Project - Rendering Users List/src/App.js
@@ -0,0 +1,18 @@
+// Write your code at relevant places in the code below:
+
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  return (
+    <div>
+      <AddUser />
+      <UsersList users={users} />
+    </div>
+  );
+}
+
+export default App;
